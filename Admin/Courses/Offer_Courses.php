8d73a4d20db87086db1f4611f9d2777c5e509baf
Al Mahmud Riaz <almahmudr1@gmail.com>
2020-10-22 03:00:56 +0600

    Courses -> Add, Edit, Delete!

--- Admin/Courses/Offer_Courses.php
+++ Admin/Courses/Offer_Courses.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (isset($_SESSION['username']) && isset($_SESSION['user_type'])) {
-
+require "../../include/conn.php";
  ?>
 
 <!DOCTYPE html>
@@ -10,7 +10,7 @@ if (isset($_SESSION['username']) && isset($_SESSION['user_type'])) {
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Admin</title>
+    <title>Add New Course - Admin</title>
     <link rel="stylesheet" href="Offer_Courses.css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -75,28 +75,84 @@ if (isset($_SESSION['username']) && isset($_SESSION['user_type'])) {
 <div class="title">
 <h1 style="margin-left:42%; margin-top: 5%;"> Add New Course </h1>
 </div>
-<table style="margin-left:40%; margin-top:20px;">
-  <tr>
-    <td style="padding:10px;" >Course Name : </td>
-    <td style="padding:10px;">  <input type="text" name="" value=""> </td>
-</tr>
-<tr>
-  <td style="padding:10px;">College year:</td>
-  <td style="padding:10px;">  <select name="gender_type"  style="width:175px;" required >
-              <option value="" disabled  selected>     Select Year     </option>
-              <option value="HYEF">First</option>
-              <option value="YFEF">Second</option></td>
-</tr>
-<tr>
-  <td style="padding:10px;">Course Code: </td>
-  <td style="padding:10px;">  <input type="text" name="" value=""> </td>
-</tr>
-<tr>
-  <td align=center colspan="2" style="padding:10px;" >
-    <input class="submit" type="submit" name="submit_btn" value="Submit">
-  </td>
-</tr>
-</table>
+
+<?php
+
+if (isset($_POST['submit_btn'])){
+  $CourseName = $_POST['c_name'];
+  $CollegeYear = $_POST['college_year'];
+
+  if (empty($CourseName) || empty($CollegeYear)) {
+    ?>
+      <table style="margin-left:40%; margin-top:20px;">
+        <tr>
+          <td colspan="6" style="color:red;">*Empty Field!
+          </td>
+        </tr>
+      </table>
+    <?php
+  }else {
+    $mysql_qry = "select * from course where cname ='$CourseName' and year ='$CollegeYear'";
+    $result = mysqli_query($conn,$mysql_qry);
+        if (mysqli_num_rows($result)>0) {
+          ?>
+            <table style="margin-left:40%; margin-top:20px;">
+              <tr>
+                <td colspan="6" style="color:red;">Course Name Already Exists!</td>
+              </tr>
+            </table>
+          <?php
+        }else {
+          $sql = "INSERT INTO course (cname, year,code) VALUES ('$CourseName', '$CollegeYear','1001')";
+          if ($conn->query($sql) === TRUE) {
+            ?>
+              <table style="margin-left:40%; margin-top:20px;">
+                <tr>
+                  <td colspan="6" style="color:green;">New Course Added!</td>
+                </tr>
+              </table>
+            <?php
+          }else {
+            ?>
+              <table style="margin-left:40%; margin-top:20px;">
+                <tr>
+                  <td colspan="6" style="color:red;">Something Wrong! <br>Try Again Please!</td>
+                </tr>
+              </table>
+            <?php
+          }
+        }
+  }
+
+}
+
+ ?>
+
+<form action="" method="post">
+  <table style="margin-left:40%; margin-top:20px;">
+    <tr>
+      <td style="padding:10px;" >Course Name : </td>
+      <td style="padding:10px;">  <input type="text" name="c_name" value="" required> </td>
+    </tr>
+    <tr>
+      <td style="padding:10px;">College year:</td>
+      <td style="padding:10px;">  <select name="college_year"  style="width:175px;" required >
+                  <option value="" disabled  selected>     Select Year     </option>
+                  <option value="First">First</option>
+                  <option value="Second">Second</option>
+      </td>
+    </tr>
+    <tr>
+      <td style="padding:10px;">Course Code: </td>
+      <td style="padding:10px;">  <input type="text" name="course_code" value=""> </td>
+    </tr>
+    <tr>
+      <td align=center colspan="2" style="padding:10px;" >
+        <input class="submit" type="submit" name="submit_btn" value="Submit">
+      </td>
+    </tr>
+  </table>
+</form>
 
 
 </body>
